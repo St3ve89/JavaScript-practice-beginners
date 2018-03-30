1e7b7899000cba55ccee91dd0170a1623b17e5b2
St3ve89 <istvan.acs89@gmail.com>
2018-03-30 17:18:20 +0100

    Add changes

---
+++ Variable and String challenge/js.js
@@ -0,0 +1,9 @@
+alert("Welcome User!");
+
+var VisitorName = prompt ("What is your name?");
+
+var VisitorColor = prompt ("What is your favourite color?");
+
+document.write("Hello " + VisitorName + "Your Favourite Color Is " + VisitorColor)
+
+console.log("Challenge completed!");
